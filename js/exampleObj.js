fb084abd309197f6f12cb207ea3adb983cb6c0eb
Danila-hub <KonTaigaBox@mail.ru>
2021-11-19 15:36:57 +0300

    Issue #12 | Added second example for obj-fit

--- js/exampleObj.js
+++ js/exampleObj.js
@@ -12,6 +12,19 @@ let leftExampleText = `<div class="preview__objectFitRow">
 </div>
 `;
 
+let leftExampleTextSecond = `<div class="preview__objectFitRow">
+    <div class="preview__objectFitItem">
+        <img src="../path/YourImage.jpeg" alt="" />
+    </div>
+    <div class="preview__objectFitItem">
+        <img src="../path/YourImage.jpeg" alt="" />
+    </div>
+    <div class="preview__objectFitItem">
+        <img src="../path/YourImage.jpeg" alt="" />
+    </div>
+</div>
+`;
+
 let rightExampleText = `.preview__objectFitRow {
   display: flex;
   flex-flow: row nowrap;
@@ -24,6 +37,34 @@ let rightExampleText = `.preview__objectFitRow {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+/* If you are using a smartphone 
+with a display resolution of up 
+to 556px you see this example:  */
+@media screen and (max-width: 566px) {
+  .preview__objectFitRow {
+    display: flex;
+    flex-flow: column nowrap;
+  }
+  .preview__objectFitItem {
+    height: 170px;
+    padding: 5px 5px;
+  }
+}
+`;
+
+let rightExampleTextSecond = `.preview__objectFitRow {
+  display: flex;
+  flex-flow: row nowrap;
+}
+.preview__objectFitItem {
+  height: 300px;
+  padding: 0 5px;
+}
+.preview__objectFitItem img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 /* If you are using a smartphone 
 with a display resolution of up 
@@ -59,8 +100,14 @@ document.querySelectorAll("code").forEach(function (element) {
 let leftExample = document.getElementById("htmlExample");
 leftExample.textContent = leftExampleText;
 
+let leftExampleSecond = document.getElementById("htmlExampleSecond");
+leftExampleSecond.textContent = leftExampleTextSecond;
+
 let rightExample = document.getElementById("cssExample");
 rightExample.textContent = rightExampleText;
+
+let rightExampleSecond = document.getElementById("cssExampleSecond");
+rightExampleSecond.textContent = rightExampleTextSecond;
 
 //==================================================//
 
@@ -85,6 +132,25 @@ copyHtmlButton.onclick = function () {
   );
 };
 
+let copyHtmlButtonSecond = document.getElementById("copyHtmlButtonSecond");
+copyHtmlButtonSecond.onclick = function () {
+  navigator.clipboard.writeText(leftExampleTextSecond).then(
+    function () {
+      copyHtmlButtonSecond.textContent = "Copied!";
+      setTimeout(() => {
+        copyHtmlButtonSecond.textContent = `Copy`;
+        let iconFont = document.createElement("span");
+        iconFont.setAttribute("class", "icon-copyIcon copyButtonIcon");
+        console.log(iconFont);
+        copyHtmlButtonSecond.appendChild(iconFont);
+      }, 1000);
+    },
+    function () {
+      //If fail//
+    }
+  );
+};
+
 let copyCssButton = document.getElementById("copyCssButton");
 copyCssButton.onclick = function () {
   navigator.clipboard.writeText(rightExampleText).then(
@@ -96,6 +162,24 @@ copyCssButton.onclick = function () {
         iconFont.setAttribute("class", "icon-copyIcon copyButtonIcon");
         console.log(iconFont);
         copyCssButton.appendChild(iconFont);
+      }, 1000);
+    },
+    function () {
+      //If fail//
+    }
+  );
+};
+let copyCssButtonSecond = document.getElementById("copyCssButtonSecond");
+copyCssButtonSecond.onclick = function () {
+  navigator.clipboard.writeText(rightExampleTextSecond).then(
+    function () {
+      copyCssButtonSecond.textContent = "Copied!";
+      setTimeout(() => {
+        copyCssButtonSecond.textContent = `Copy`;
+        let iconFont = document.createElement("span");
+        iconFont.setAttribute("class", "icon-copyIcon copyButtonIcon");
+        console.log(iconFont);
+        copyCssButtonSecond.appendChild(iconFont);
       }, 1000);
     },
     function () {
